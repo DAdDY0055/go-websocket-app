9ad1d18ac6d17cc11e954db40aa9114fa535b3c2
Takuya <yky2800.rthm.55tn@gmail.com>
2020-01-18 18:09:19 +0900

    テストの作成

---
+++ trace/tracer.go
@@ -0,0 +1,24 @@
+package trace
+import (
+	"fmt"
+	"io"
+)
+
+// Tracerはコード内での出来事を記録できるオブジェクトを表すインターフェース
+// Tracer型は先頭のTが大文字なので公開される
+type Tracer interface {
+	Trace(...interface{}) // 引数をいつくでも受け取り可能
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
