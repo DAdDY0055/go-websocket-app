f5a7aa357dcba4ca3de05cfcf17d83240c380d8f
Takuya <yky2800.rthm.55tn@gmail.com>
2020-07-05 10:24:22 +0900

    GoModuleを入れた。

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/DAdDY0055/go-websocket-app
+
+go 1.13
+
+require github.com/gorilla/websocket v1.4.2
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"log"
-	"net/http"	
+	"net/http"
 	"text/template"
 	"path/filepath"
 	"sync"
